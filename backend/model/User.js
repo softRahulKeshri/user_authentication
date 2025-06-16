ef44e414553685f9ce32ed25c37372b9b1a40f05
softRahulKeshri <rahul.k@magureinc.com>
2025-06-16 17:44:29 +0530

    add: bridge between backend and mongodb

---
+++ backend/model/User.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+// Schema
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    default: null,
+  },
+  lastName: {
+    type: String,
+    default: null,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+// Model
+module.exports = mongoose.Model("user", userSchema);
